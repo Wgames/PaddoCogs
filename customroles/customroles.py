b371dab4125952d597c068723a1997d60a5e32e8
Rowien Bolkensteyn <rbo@wgames.ch>
2017-04-08 11:46:08 +0200

    remove msg on apply CustomRoles

--- customroles/customroles.py
+++ customroles/customroles.py
@@ -82,7 +82,7 @@ class CustomRoles:
                     message = 'No such role'
         else:
             message = 'There is no such role on this server'
-        await self.bot.say(message)
+        #await self.bot.say(message)
 
     @_role.command(pass_context=True, no_pm=True, name='relieve')
     async def _relieve(self, context, *role_name):
